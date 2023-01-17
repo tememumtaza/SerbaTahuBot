7254f7c6ac634f6efff00fb86dc42d8de1ee7939
tememumtaza <43274888+tememumtaza@users.noreply.github.com>
2023-01-17 17:54:00 +0700

    Adding Open AI API Key & Bot name

--- amadeus.py
+++ amadeus.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "Your OpenAI key"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "Your Telegram Key"
+    tgkey = "5893196257:AAHElvT8N_Nr9PyHWvs75pd2RcyBmZZXiy0"
 else:
     tgkey = tgenv
 print(tgenv)
@@ -35,8 +35,8 @@ running = False
 cache = None
 qcache = None
 chat_log = None
-botname = 'Makise Kurisu'
-username = 'Your name here'
+botname = 'Serba Tahu'
+username = 'SerbaTahuBot'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
