0ba3d3bda9fcfa3ef3d5e2aba7771e485a3a882f
tememumtaza <43274888+tememumtaza@users.noreply.github.com>
2023-01-17 18:28:32 +0700

    Update amadeus.py

--- amadeus.py
+++ amadeus.py
@@ -66,8 +66,8 @@ def start(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Makise Kurisu'
-        username = 'thewickedkarma'
+        botname = 'Serba Tahu'
+        username = 'SerbaTahuBot'
         update.message.reply_text('Hi')
         return 
     else:
@@ -93,16 +93,16 @@ def reset(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Makise Kurisu'
-        username = 'thewickedkarma'
+        botname = 'Serba Tahu'
+        username = 'SerbaTahuBot'
         update.message.reply_text('Bot has been reset, send a message!')
         return
     if tim == 1:
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Makise Kurisu'
-        username = 'thewickedkarma'
+        botname = 'Serba Tahu'
+        username = 'SerbaTahuBot'
         update.message.reply_text('Bot has been reset, send a message!')
         return 
     else:
@@ -128,8 +128,8 @@ def retry(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Makise Kurisu'
-        username = 'thewickedkarma'
+        botname = 'Serba Tahu'
+        username = 'SerbaTahuBot'
         update.message.reply_text('Send a message!')
         return 
     else:
